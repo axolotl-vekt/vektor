5b71ac30161f27cac6eaa1f7dcf602ae598bb253
user-byte123 <ewong1217@gmail.com>
2023-12-23 13:48:55 -0800

    added imports into server.js

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const path = require('path');
 const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/',router);
+
+app.get('/', (res,req) => {
+
+});
