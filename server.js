ef647d7d7fa7de94ffe8dd4f6c721f8a5c3bacf9
user-byte123 <ewong1217@gmail.com>
2023-12-23 14:07:04 -0800

    started server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const express = require('express');
+const router = require('./router/router.js');
+const controller = require('./controller/controller.js');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
-
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(cors())
+app.use(express.urlencoded({extended: true}));
 app.use('/',router);
 
 app.get('/', (res,req) => {
