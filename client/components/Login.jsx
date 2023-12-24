e721c170be895137bdf39e6e0b54f0c1b73ee8cb
user-byte123 <ewong1217@gmail.com>
2023-12-23 16:22:34 -0800

    added login and styles

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,6 +1,43 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+
+
 function Login() {
+    const [ loginData, setLogin ] = useState([]);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+    }
+        
     return(
-        <div>Hello</div>
+        <div>
+            <div className='loginContainer'>
+                <div className='loginCard'>
+                    <h2 className='loginTitle'>Login</h2>
+                    <p>Don't have an account? <a href="">SignUp</a></p>
+                    <form className='loginInputForm'>
+                        <div className='loginLabelDiv'>
+                            <label >Username: </label>
+                        </div>
+                        <div >
+                            <input className='loginInput' type='text' onChange={handleChange}></input>
+                        </div>
+                        <div className='loginLabelDiv'>
+                            <label >Password: </label>
+                        </div>
+                        <div>
+                            <input className='loginInput' type='text' onChange={handleChange}></input>
+                        </div>
+                        <div className='loginBtn'>
+                            <Link to='/homepage'>
+                            <button>Sign in</button>
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Login;
