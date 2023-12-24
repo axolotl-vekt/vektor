5a82bd9b87ed57ce0dc0350c2a1fbb1bc1f19a0c
King-Hur Wu <king2510@gmail.com>
2023-12-23 18:33:12 -0600

    updated userModel schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: { type: String},
+  lastName: { type: String},
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
