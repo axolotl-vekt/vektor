fda6321316e7967326d59de660c2e3ac85152d1b
King-Hur Wu <king2510@gmail.com>
2023-12-27 16:41:44 -0600

    updated foodlog componenet

--- client/components/InfoCard.jsx
+++ client/components/InfoCard.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 
 function InfoCard() {
+
+
+
+
+
+
+
   return (
     <div className='entries-container'>
       Entries
