a73405b389df76f8c13cbaea4f973b9d7792ce3e
Amir <amirm034@gmail.com>
2024-01-02 20:18:21 -0800

    240102_Routing Implemt. & Troubleshooting_2017_AM > > Co-Author: Solomon Moon chochobo91@gmail.com Co-Author: Amir Munoz amirm034@gmail.com Co-Author: Jarod Crawford crawfojm04@gmail.com Co-Author: Sean Nguyen seannguyennn@gmail.com

---
+++ server/models/_sessionModel.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const sessionSchema = new Schema({
+//   cookieId: { type: String, required: true, unique: true },
+//   createdAt: { type: Date, expires: 30, default: Date.now }
+// });
+
+
+// const Session = mongoose.model('Session', sessionSchema);
+// module.exports = { Session };
+
+
