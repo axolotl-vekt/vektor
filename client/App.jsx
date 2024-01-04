27024b21af5821ef1e6f7b9b72383de9660077f0
Jarod Crawford <jarodcrawford@Jarods-Air.attlocal.net>
2024-01-04 12:11:54 -0600

    24/01/03_Jarod_LandingPage_JC_1211_JC

--- client/App.jsx
+++ client/App.jsx
@@ -6,12 +6,14 @@ import SignUp from './components/SignUp';
 import FoodLog from './components/FoodLog';
 import Games from './components/Games';
 import MemoryGame from './components/MemoryGame';
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
     <Routes>
-      <Route exact path='/' element={<Login />} />
+      <Route exact path='/' element={<LandingPage />} />
       <Route exact path='/homepage' element={<Homepage />} />
+      <Route exact path='/signin' element={<Login />} />
       <Route exact path='/signup' element={<SignUp />} />
       <Route exact path='/foodlog' element={<FoodLog />} />
       <Route exact path='/games' element={<Games />}/>
