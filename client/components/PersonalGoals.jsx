dad16a62faa48322acf724865bd77d52e72c6b4c
user-byte123 <ewong1217@gmail.com>
2024-08-04 23:09:32 -0700

    personalGoals accordian

--- client/components/PersonalGoals.jsx
+++ client/components/PersonalGoals.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Navbar from './Navbar'
+import AccordionRepeat from './Features/AccordionRepeat.jsx'
 
 export default function PersonalGoals() {
   return (
     <div>
-      <div>
-        <Navbar/>
+      <Navbar/>
+      <div className='flow content-center place-content-center h-60 w-60'>
+        <AccordionRepeat
+        summary={<div>Diabetes Goals</div>}
+        details={<div>Sugar Goals:  <input className='border-2 border-solid border-black rounded-lg'></input></div>}
+        />
+        <AccordionRepeat
+          summary={<div>Hypertension Goals</div>}
+          details={<div>Blood Pressure Goals: <input/></div>}
+        />
+        <AccordionRepeat
+          summary={<div>Exercise Goals</div>}
+          details={<div>Exercise Goals: </div>}
+        />
       </div>
     </div>
   )
